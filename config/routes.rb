7e66038eda9b6ab3f847ab28c347b5f776106f20
Adewale Orotayo <didymus7007@gmail.com>
2020-07-17 13:55:37 +0100

    Allow a user to create todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :todos, only: [:index, :new, :create]
+
   root "todos#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+  scenario 'successfully' do
+    visit root_path
+
+    click_on 'New Todo'
+    fill_in "Title", with: 'Trying out todo'
+    click_on "Add Todo"
+
+    expect(page).to have_css '.todos li', text: 'Trying out todo'
+  end
+end
