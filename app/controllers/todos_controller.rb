5cc5ed20ef14e9475dc4a83183013af6505c3fb1
Adewale Orotayo <didymus7007@gmail.com>
2020-07-21 14:57:07 +0100

    Refactored to using User object for presence of the user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: current_email)
+    @todos = current_user.todos
   end
 
   def new
@@ -10,7 +10,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    Todo.create(todo_params.merge(email: current_email))
+    current_user.todos.create(todo_params)
     
     redirect_to todos_path
   end
